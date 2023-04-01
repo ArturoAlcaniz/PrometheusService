78c5a3ee7e027aa10b4e2dec88b27693756e6285
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-04-01 23:11:00 +0000

    fix replace envs

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@
 # Script to be executed just once, when the system is installed.
 # Commands are only run once even if the script is called multiple times.
 dockerd > /dev/null 2>&1 &
-/app/PrometheusService/replace-envs.bash &
 /app/PrometheusService/prometheus/node_exporter/node_exporter --web.listen-address=":9100" >/dev/null 2>&1 & /app/PrometheusService/prometheus/prometheus --config.file=/app/PrometheusService/prometheus/prometheus.yml --enable-feature=expand-external-labels
