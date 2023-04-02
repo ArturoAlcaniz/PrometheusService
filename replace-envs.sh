6f5826b3e49b4f9feae73530d9f3fba35bffe2f1
ArturoAlcaniz <arturoguijarro@hotmail.com>
2023-04-02 14:09:06 +0000

    added metrics for products

--- replace-envs.sh
+++ replace-envs.sh
@@ -3,7 +3,11 @@
 # Obtener los valores de las variables de entorno
 USERS_CONTAINER_NAME=$USERS_CONTAINER_NAME
 USERS_CONTAINER_PORT=$USERS_CONTAINER_PORT
+PRODUCTS_CONTAINER_NAME=$PRODUCTS_CONTAINER_NAME
+PRODUCTS_CONTAINER_PORT=$PRODUCTS_CONTAINER_PORT
 
 # Reemplazar las variables en el archivo prometheus.yml
 sed -i "s/\${USERS_CONTAINER_NAME}/$USERS_CONTAINER_NAME/g" /app/PrometheusService/prometheus/prometheus.yml
 sed -i "s/\${USERS_CONTAINER_PORT}/$USERS_CONTAINER_PORT/g" /app/PrometheusService/prometheus/prometheus.yml
+sed -i "s/\${PRODUCTS_CONTAINER_NAME}/$PRODUCTS_CONTAINER_NAME/g" /app/PrometheusService/prometheus/prometheus.yml
+sed -i "s/\${PRODUCTS_CONTAINER_PORT}/$PRODUCTS_CONTAINER_PORT/g" /app/PrometheusService/prometheus/prometheus.yml
